dd142a4e95725a029d6bb25275949626933ddb44
Aman <lapi.work.2019@gmail.com>
2021-08-01 22:53:30 -0700

    loading and error

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 
+import ActivityIndicator from '../components/ActivityIndicator';
+import Button from '../components/Button';
 import Card from '../components/Card';
 import colors from '../config/colors';
 import Screen from '../components/Screen';
 import routes from '../navigation/routes';
 import listingApi from '../api/listings';
+import AppText from '../components/Text';
 
 // const listings = [
 // 	{
@@ -24,18 +27,43 @@ import listingApi from '../api/listings';
 
 function ListingsScreen({ navigation }) {
 	const [listings, setListings] = useState([]);
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 		loadListings();
 	}, []);
 
 	const loadListings = async () => {
+		setLoading(true);
 		const response = await listingApi.getListings();
+		setLoading(false);
+
+		if (!response.ok) return setError(true);
+
+		setError(false);
 		setListings(response.data);
 	};
 
 	return (
 		<Screen style={styles.screen}>
+			{error && (
+				<View
+					style={{
+						justifyContent: 'center',
+						alignItems: 'center',
+						top: '40%',
+					}}
+				>
+					<AppText>Couldn't retrive your Listings Feed.</AppText>
+					<Button
+						title="Retry"
+						sideicon="searchengin"
+						onPress={loadListings}
+					></Button>
+				</View>
+			)}
+			{/* <ActivityIndicator visible={loading} /> */}
 			<FlatList
 				data={listings}
 				keyExtractor={(listing) => listing.id.toString()}
